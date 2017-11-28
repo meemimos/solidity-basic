e972c2fa699f5e8a732e04720798e89cc750332a
meemimos <sayabcz@hotmail.com>
2017-11-28 12:16:34 +0545

    fixes #2

--- HelloWorldContract/HelloWorldContract.sol
+++ HelloWorldContract/HelloWorldContract.sol
@@ -3,7 +3,15 @@ pragma solidity ^0.4.0;
 contract HelloWorldContract {
     string word = "Hello World";
     
+    //getter
     function getWord() constant returns(string) {
             return word;
-        }
+    }
+
+    //setter
+    function setWord(string newWord) returns(string) {
+        word = newWord;
+        return word;
+    }
+    
 }

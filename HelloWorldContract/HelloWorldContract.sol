16dee50f1227c29eab24396a78ef748db601ad13
meemimos <sayabcz@hotmail.com>
2017-11-28 12:48:38 +0545

    fixes #3

--- HelloWorldContract/HelloWorldContract.sol
+++ HelloWorldContract/HelloWorldContract.sol
@@ -1,17 +1,28 @@
 pragma solidity ^0.4.0;
 
 contract HelloWorldContract {
-    string word = "Hello World";
-    
-    //getter
-    function getWord() constant returns(string) {
-            return word;
-    }
+    string public word = "Hello World";
 
-    //setter
-    function setWord(string newWord) returns(string) {
-        word = newWord;
+    address issuer;
+    
+    function HelloWorldContract() {
+        issuer = msg.sender;
+    }
+    
+    function getWord() constant returns(string) {
         return word;
     }
     
+    modifier ifIssuer() {
+        if (issuer != msg.sender) {
+            throw;
+        } else {
+            _;
+        }
+    }
+    
+    function setWord(string newWord) ifIssuer returns(string) {
+        word = newWord;
+        return word;
+    }
 }

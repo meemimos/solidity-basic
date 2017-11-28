650dc07638241de6f394f8e7f6b888e11719d015
meemimos <sayabcz@hotmail.com>
2017-11-28 14:17:43 +0545

    event

--- CustodialContract/CustodialContract.sol
+++ CustodialContract/CustodialContract.sol
@@ -4,6 +4,9 @@ contract CustodialContract {
     address client;
     bool _switch;
 
+    event UpdateStatus(string _msg, uint _amount);
+    event UserStatus(string _msg, address user, uint _amount);
+
     function CustodialContract() {
         client = msg.sender;
     }
@@ -16,17 +19,19 @@ contract CustodialContract {
         }
     }
 
+    //payble function
     function depositeFunds() payable {
-
+        UserStatus("User deposite some money", msg.sender, msg.value);
     }
 
+    //this is not a payable function so we cannot use msg.value or msg.sender 
     function withdrawFunds(uint amount) ifClient {
         if (client.send(amount)) {
+            UpdateStatus("User withdraw some money", amount);
             _switch = true;
         } else {
             _switch = false;
         }
-
     }
 
     function getFunds() ifClient constant returns(uint) {
